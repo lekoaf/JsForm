d9d0cd17b4d20256fa4448ebe5f97e02b6e9a28b
Martin Trobäck <martin.troback@outlook.com>
2016-03-04 22:45:14 +0100

    Updated default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
 
-gulp.task('default', function() {
-  console.log('Nothing here!');
-});
+gulp.task('default', ['compress']);
 
 gulp.task('compress', function() {
   gulp.src('src/jsform.js')
