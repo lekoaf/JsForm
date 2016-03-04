2f78a45fefed07655be564ea5bd38e137db67a61
Martin Trobäck <martin.troback@outlook.com>
2016-03-05 00:50:40 +0100

    Custom gulp task for bumping version

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
 var plumber = require('gulp-plumber');
+var exec = require('child_process').exec;
+var git = require('gulp-git');
 
 gulp.task('default', ['minify']);
 
@@ -18,4 +20,61 @@ gulp.task('minify', function() {
 
 gulp.task('watch', function () {
   gulp.watch('src/jsform.js', ['minify']);
+});
+
+gulp.task('tag', function () {
+  var typeIndex = process.argv.indexOf('--type');
+  var msgIndex = process.argv.indexOf('--msg');
+  var type, msg, version;
+  var child = exec('git describe --tags', function (error, stdout, stderr) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    if (typeIndex !== -1) {
+      type = process.argv[typeIndex + 1]
+    } else {
+      type = 'patch';
+    }
+
+    version = stdout.trim().split('.');
+
+    if (!version) {
+      version = [0,0,0];
+    }
+
+    switch (type) {
+      case 'major':
+        version[0] = parseInt(version[0]) + 1;
+        break;
+      case 'minor':
+        version[1] = parseInt(version[1]) + 1;
+        break;
+      default:
+        version[2] = parseInt(version[2]) + 1;
+        break;
+    }
+
+    version = version.join('.');
+
+    if (msgIndex !== -1) {
+      msg = process.argv[msgIndex + 1];
+    } else {
+      msg = 'Bumping version';
+    }
+
+    git.tag(version, msg, function (err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      git.push('origin', version, function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+    });
+  });
 });
